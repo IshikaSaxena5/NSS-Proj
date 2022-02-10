8771c4242513a1ecb3c4ccf4b97441dcbdeb0b0b
Ishika Saxena <ishikasaxena05032002@gmail.com>
2022-02-11 01:20:33 +0530

    Update

--- mern/server/paymentRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const PaytmChecksum = require('./PaytmChecksum')
-router.post('/payment',(req,res)=>{
-
-})
-module.exports=router;
